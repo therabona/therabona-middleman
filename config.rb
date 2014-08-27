c38268be60b08baaa331d450d0ff646774f7e36c
Wafiq Rodzuan <wafiqrodzuan@gmail.com>
2014-08-28 03:01:25 +0800

    remove category n tag link

--- config.rb
+++ config.rb
@@ -64,10 +64,6 @@ activate :blog do |blog|
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
   # blog.default_extension = ".markdown"
-
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
-
   # Enable pagination
   # blog.paginate = true
   # blog.per_page = 10
